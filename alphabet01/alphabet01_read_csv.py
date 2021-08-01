0fe3b02a7d66cad19067e511c2e9e36874380eea
SEONGMIN-96 <sm961121@gmail.com>
2021-08-01 19:39:35 +0900

    mnist_alphabet

---
+++ alphabet01/alphabet01_read_csv.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# data = pd.read_csv('./_data/alphabet_data/A_Z Handwritten Data.csv')
+# data = data.to_numpy()
+
+# print(data.shape)   # (372450, 785)
+# print(type(data))   # <class 'numpy.ndarray'>
+
+# np.save('./_save/_npy/alphabet_data_01.npy', arr=data)
+
+# 알파벳 .csv -> .npy 저장
+
+# data = np.load('./_save/_npy/alphabet_data_01.npy')
+
+# print(data.shape)   # (372450, 785)
+# print(type(data))   # <class 'numpy.ndarray'>
+
+# 현재 28*28픽셀 크기로 이루어져 있음
+# 26개의 구분된 열이 존재
+
