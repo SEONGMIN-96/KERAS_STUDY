aa025bf473b237c13a4bdb2f0e9ab3ee7c02d5ee
SEONGMIN-96 <sm961121@gmail.com>
2021-08-02 14:09:15 +0900

    traffic_accident_preprocessing

--- traffic_accident_01/t_accident_01.py
+++ traffic_accident_01/t_accident_01.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import openpyxl
+import csv
 
 # 1. 데이터
 
@@ -23,4 +23,144 @@ file_t_accident_2017 = '사고건수_사망자수_부상자수/traffic_accident_
 file_t_accident_2018 = '사고건수_사망자수_부상자수/traffic_accident_seoul_2018.xls'
 file_t_accident_2019 = '사고건수_사망자수_부상자수/traffic_accident_seoul_2019.xls'
 file_t_accident_2020 = '사고건수_사망자수_부상자수/traffic_accident_seoul_2020.xls'
- 
+
+# pandas.errors.ParserError: Error tokenizing data. C error: Expected 1 fields in line 4, saw 2
+# rain_data = pd.read_csv(file_path+file_rain, delimiter=';', header=None,)
+
+# 데이터 전처리
+
+# 강수량
+
+f = open(file_path+file_rain, 'r', encoding='cp949')
+data = list(csv.reader(f, delimiter=','))
+data = data[12:len(data)]
+
+df = pd.DataFrame(data[1:4019])
+print(df.loc[:,[2,3]])
+df = df.loc[:,[3]]
+print(df.info())
+data = np.array(df)
+print(data.shape) # (4018, 1)
+print(type(data[1][0])) # <class 'str'>
+
+# 강수량 empty = '' -> empty = '0'
+# 데이터를 str -> int
+
+data_empty = np.where(data == '')
+df.loc[data_empty[0],:] = '0'
+data = np.array(df)
+
+print(type(data[0][0]))
+print(data[0].shape)
+print(data[19][0])
+# data = float(data[19][0])
+# print(data)
+
+def to_float(a):
+    aaa = []
+    for i in range(len(a)):
+        subset = float(a[i][0])
+        aaa.append(subset)
+    return np.array(aaa)
+data = to_float(data)
+
+data_rain = data
+
+# 강수량 데이터 배열화 완료.
+
+# 기온
+
+f = open(file_path+file_temp, 'r', encoding='cp949')
+data = list(csv.reader(f, delimiter=','))
+data = data[12:len(data)]
+
+df = pd.DataFrame(data[0:4018])
+print(df.loc[:,[2,3]])
+df = df.loc[:,[3]]
+print(df.info())
+data = np.array(df)
+print(data) # 
+print(type(data[1][0])) # <class 'str'>
+
+# 데이터를 str -> int
+
+data_empty = np.where(data == '')
+df.loc[data_empty[0],:] = '0'
+data = np.array(df)
+
+def to_float(a):
+    aaa = []
+    for i in range(len(a)):
+        subset = float(a[i][0])
+        aaa.append(subset)
+    return np.array(aaa)
+data = to_float(data)
+
+data_temp = data
+
+# 기온 데이터 배열화 완료.
+
+# 바람
+
+f = open(file_path+file_wind, 'r', encoding='cp949')
+data = list(csv.reader(f, delimiter=','))
+data = data[12:len(data)]
+
+df = pd.DataFrame(data[4:4022])
+print(df.loc[:,[2,3]])
+df = df.loc[:,[3]]
+print(df.info())
+data = np.array(df)
+print(data) # 
+print(type(data[1][0])) # <class 'str'>
+
+# 데이터를 str -> int
+
+data_empty = np.where(data == '')
+df.loc[data_empty[0],:] = '0'
+data = np.array(df)
+
+def to_float(a):
+    aaa = []
+    for i in range(len(a)):
+        subset = float(a[i][0])
+        aaa.append(subset)
+    return np.array(aaa)
+data = to_float(data)
+
+data_wind = data
+
+# 바람 데이터 배열화 완료.
+
+# 습도
+
+f = open(file_path+file_humidity, 'r', encoding='cp949')
+data = list(csv.reader(f, delimiter=','))
+data = data[12:len(data)]
+
+df = pd.DataFrame(data[5:4023])
+print(df.loc[:,[2,3]])
+df = df.loc[:,[3]]
+print(df.info())
+data = np.array(df)
+print(data) # 
+print(type(data[1][0])) # <class 'str'>
+
+# 데이터를 str -> int
+
+data_empty = np.where(data == '')
+df.loc[data_empty[0],:] = '0'
+data = np.array(df)
+
+def to_float(a):
+    aaa = []
+    for i in range(len(a)):
+        subset = float(a[i][0])
+        aaa.append(subset)
+    return np.array(aaa)
+data = to_float(data)
+
+data_humidity = data
+
+# 습도 데이터 배열화 완료.
+
