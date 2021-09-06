1e4a71c4cd8fd7d04fe557df9ea25c3f73021591
SEONGMIN-96 <sm961121@gmail.com>
2021-09-06 20:43:23 +0900

    d

--- keras/keras75_distribute.py
+++ keras/keras75_distribute.py
@@ -1,3 +1,13 @@
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus :
+    try :
+        tf.config.experimental.set_visible_devices([gpus[0], gpus[1]] 'GPU')
+
+    except RuntimeError as e:
+        print(e)
+
+
 from tensorflow.keras.datasets import mnist
 import numpy as np
 import tensorflow as tf
